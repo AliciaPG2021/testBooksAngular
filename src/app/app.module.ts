13c83cf3294722c825747870b595ea933d27e08b
Alicia <aliciapradag@gmail.com>
2021-10-09 12:49:41 +0200

    visualizar todos los autores y crear nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { DetailComponent } from './components/detail/detail.component';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { AuthorsComponent } from './components/authors/authors.component';
 import { AuthorsFormularioComponent } from './components/authors-formulario/authors-formulario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import { AuthorsFormularioComponent } from './components/authors-formulario/auth
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
