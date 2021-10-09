b8addc94a53e7cff8e7453bffa783df3c8951814
Alicia <aliciapradag@gmail.com>
2021-10-09 10:15:12 +0200

    Creación proyecto y componentes básicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './components/books/books.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { AuthorsComponent } from './components/authors/authors.component';
+import { AuthorsFormularioComponent } from './components/authors-formulario/authors-formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooksComponent,
+    DetailComponent,
+    FormularioComponent,
+    AuthorsComponent,
+    AuthorsFormularioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/authors-formulario/authors-formulario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-authors-formulario',
+  templateUrl: './authors-formulario.component.html',
+  styleUrls: ['./authors-formulario.component.css']
+})
+export class AuthorsFormularioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
